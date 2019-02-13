02c31def87b765071a77c042aaad432d1ee50bef
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2019-02-13 18:33:07 -0500

    ashawley/service-worker@beae370

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20190210143911";
+var CACHE_NAME = "pixyll2-20190213183031";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/pixyll/css/pixyll.css?201902101439",
+        "/pixyll/css/pixyll.css?201902131830",
         "/pixyll/"
       ]);
     })
@@ -29,7 +29,23 @@ self.addEventListener("activate", function(e) {
 addEventListener("fetch", function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
-        return response || fetch(e.request)
+        return response || fetch(e.request).then(function(response) {
+        var clonedResponse = response.clone();
+        var hosts = [
+          "https://fonts.googleapis.com",
+          "https://fonts.gstatic.com",
+          "https://maxcdn.bootstrapcdn.com",
+          "https://cdnjs.cloudflare.com"
+        ];
+        hosts.map(function(host) {
+          if (e.request.url.indexOf(host) === 0) {
+            caches.open(CACHE_NAME).then(function(cache) {
+              cache.put(e.request, clonedResponse);
+            });
+          }
+        });
+        return response;
+      });
     })
   );
 });
