13ca8d0513eee97dccb01db7212ee036de9593c3
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2020-03-15 01:22:47 -0400

    develop@af7318c

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/pixyll/assets/css/main.css?202003150105",
+        "/pixyll/assets/css/main.css?202003150122",
         "/pixyll/"
       ]);
     })
